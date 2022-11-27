cf48c0f83ead3b3c616954309db89de9076178c9
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-11-26 19:56:39 -0800

    added css to edit product

--- project/react-app/src/components/EditProduct/EditProduct.js
+++ project/react-app/src/components/EditProduct/EditProduct.js
@@ -61,20 +61,24 @@ const EditProduct = () => {
 
     return (
         <>
-        {user && <div className="form-wrapper">
-            <h1>Edit a listing</h1>
-            <form className="form-container" onSubmit={submit}>
+        <div className="product-form-wrapper">
+            <div className="product-form-title">Edit a listing</div>
+            <div className="product-form-title2">Add some details about your item. Fill out what you can for now—you’ll be able to edit this later.</div>
+            <div className="product-form-title3">Listing details</div>
+            <div className="product-form-title4">Tell the world all about your item and why they'll love it.</div>
+            <form className="product-form-container" onSubmit={submit}>
                 <div className="errors">
                     {errors.length > 0 &&
                         errors.map((error) => <li key={error}>{error}</li>)}
                 </div>
-                <span>Name*</span>
+                <label className='product-form-label'>Name*</label>
                 <input
+                    className="product-form-input"
                     type="text"
                     value={name}
                     onChange={(e) => setName(e.target.value)}
                 />
-                <span>Category*</span>
+                <label className='product-form-label'>Category*</label>
                 <select
                     name={category}
                     value={category}
@@ -91,36 +95,42 @@ const EditProduct = () => {
                         </option>
                     ))}
                 </select>
-                <span>Description*</span>
+                <label className='product-form-label'>Description*</label>
                 <textarea
                     type="text"
                     value={description}
                     onChange={(e) => setDescription(e.target.value)}
                 />
-                <span>Price*</span>
+                <label className='product-form-label'>Price*</label>
                 <input
+                    className="product-form-input"
                     type="text"
                     value={price}
                     onChange={(e) => setPrice(e.target.value)}
                 />
-                <span>Quantity*</span>
+                <label className='product-form-label'>Quantity*</label>
                 <input
+                    className="product-form-input"
                     type="number"
                     value={quantity}
+                    min="1"
                     onChange={(e) => setQuantity(e.target.value)}
                 />
-                <span>Image*</span>
+                <label className='product-form-label'>Image*</label>
                 <input
+                    className="product-form-input"
                     type="text"
                     value={image}
                     onChange={(e) => setImage(e.target.value)}
                 />
-                <button className="create-product-submit-button" type="submit">Submit</button>
-                <button type="button" className="create-product-cancel-button" onClick={cancelHandler}>
-                Cancel
-                </button>
+                <div className="product-form-buttons">
+                    <button className="product-form-submit-button" type="submit">Submit</button>
+                    <button type="button" className="product-form-cancel-button" onClick={cancelHandler}>
+                    Cancel
+                    </button>
+                </div>
             </form>
-        </div>}  
+        </div> 
     </>
   );
 };
