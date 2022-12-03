e8aad47b80c02c278d0df9e0e1be66f785d7dfc8
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-12-02 17:25:32 -0800

    added search products in components

--- react-app/src/components/SearchProduct/SearchProduct.js
+++ react-app/src/components/SearchProduct/SearchProduct.js
@@ -1,25 +1,69 @@
 import React, { useEffect} from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory } from "react-router-dom";
+import { useHistory, useParams, NavLink } from "react-router-dom";
 import { getSearchProductsThunk } from '../../store/product'
 import "./SearchProduct.css";
 
 const SearchProduct = () => {
   const dispatch = useDispatch();
   const history = useHistory();
+  const {keyword} = useParams();
 
   const user = useSelector((state) => state.session.user);
   const products = useSelector(state => state.product.searchProducts);
   const productsArr = Object.values(products);
 
   useEffect(() => {
-    dispatch(getSearchProductsThunk())
-  }, [dispatch]);
+    dispatch(getSearchProductsThunk(keyword))
+  }, [dispatch, keyword]);
 
+  if (Object.keys(productsArr).length === 0) {
+    return null;
+  }
 
+  
 return (
+    <>
     <h1>Search Product</h1>
-)
+     <div className="products-container">
+            {productsArr && productsArr.map((product) => {
+                return (
+                    <>
+                    <NavLink key={product.id} to={`/products/${product.id}`}>
+                    <div>
+                    <img
+                        className="products-image"
+                        src={product.image}
+                        alt=""
+                        onError={e => {
+                          e.currentTarget.src = "https://nckenya.com/wp-content/themes/consultix/images/no-image-found-360x260.png"
+                          e.onerror=null;
+                        }}
+                      />
+                    </div>
+                    <div className="products-info">
+                      <div className="products-name">{product.name}</div>
+                        <div className="products-rating-info">
+                          {product.avgRating % 1 ? 
+                            <div>{[...Array(Math.floor(product.avgRating))].map(star => <i className="fa-solid fa-star fa-xs"></i>)}
+                              <i className="fa-solid fa-star-half fa-xs"></i>
+                                <span className="products-reviewCount"> ({product.reviewCount})</span>
+                            </div>
+                            :
+                            <div>{[...Array(product.avgRating)].map(star => <i className="fa-solid fa-star fa-xs"></i>)}
+                              <span className="products-reviewCount"> ({product.reviewCount})</span>
+                            </div>
+                          }
+                      </div>
+                      <div className="products-price">${(product.price).toFixed(2)}</div>
+                    </div>
+                    </NavLink>
+                    </>
+                )
+            })}
+        </div>
+    </>
+  )
 
 }
 
