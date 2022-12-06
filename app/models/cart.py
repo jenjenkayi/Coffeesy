eb82e21c9279fa01ca287ff3394ba46ce4e2e60b
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-12-05 18:40:02 -0800

    added cart & search bar

---
+++ app/models/cart.py
@@ -0,0 +1,9 @@
+from .db import db, environment, SCHEMA, add_prefix_for_prod
+from datetime import datetime
+
+
+class Cart(db.Model):
+    __tablename__ = "carts"
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
