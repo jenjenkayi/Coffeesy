002e5a211f92155bfd026cf91b0b0055be682338
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-12-08 19:02:56 -0800

    added user models

--- app/models/user.py
+++ app/models/user.py
@@ -20,7 +20,8 @@ class User(db.Model, UserMixin):
 
     products = db.relationship("Product", back_populates="user")
     reviews = db.relationship("Review", back_populates="user")
-
+    cartItems = db.relationship("CartItem", back_populates="user")
+    
     @property
     def password(self):
         return self.hashed_password
