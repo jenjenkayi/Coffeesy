952f738dc9ab769dbf3439c744595534afb46f15
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-12-10 21:36:07 -0800

    add item to cart route

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -165,11 +165,23 @@ def search_product(keyword):
 @product_routes.route('/<int:productId>/cart', methods=['POST'])
 @login_required
 def add_cartItem(productId):
+    product = Product.query.get(productId)
+
+    if not product: 
+        return {"message": "Product not found", "statusCode": 404}
+
+    cartItem = db.session.query(CartItem) \
+                         .filter(CartItem.user_id == current_user.id) \
+                         .filter(CartItem.product_id == productId) \
+                         .first()
+
+
     form = CartForm()
     form['csrf_token'].data = request.cookies['csrf_token']
 
     if form.validate_on_submit():
-        new_cartItem = CartItem(
+        if not cartItem:
+            new_cartItem = CartItem(
             user_id=current_user.id,
             product_id = productId,
             quantity = form.data['quantity'],
@@ -179,5 +191,7 @@ def add_cartItem(productId):
         db.session.add(new_cartItem)
         db.session.commit()
         return jsonify(new_cartItem.to_dict())
+    else: 
+        
     return {"errors": validation_errors_to_error_messages(form.errors)}, 401
  
