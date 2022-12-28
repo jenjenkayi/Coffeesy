ad6c6d732db8210a6bcdf8399877ac888f3c7e76
Jenny Jiang <jennyjiang0830@hotmail.com>
2022-12-28 13:31:21 -0800

    remigrate/reseed

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -2,6 +2,7 @@ from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .products import seed_products, undo_products
 from .reviews import seed_reviews, undo_reviews
+from .cartItems import seed_cartItems, undo_cartItems
 
 from app.models.db import db, environment, SCHEMA
 
@@ -21,9 +22,11 @@ def seed():
         undo_users()
         undo_products()
         undo_reviews()
+        undo_cartItems()
     seed_users()
     seed_products()
     seed_reviews()
+    seed_cartItems()
     # Add other seed functions here
 
 
@@ -33,4 +36,5 @@ def undo():
     undo_users()
     undo_products()
     undo_reviews()
+    undo_cartItems()
     # Add other undo functions here
---
+++ app/seeds/cartItems.py
@@ -0,0 +1,39 @@
+from app.models import db, CartItem, environment, SCHEMA
+
+
+def seed_cartItems():
+    cartItem1 = CartItem(
+        user_id=1,
+        product_id=2,
+        quantity=1,
+    )
+    
+    cartItem2 = CartItem(
+        user_id=2,
+        product_id=1,
+        quantity=2,
+    )
+    
+    cartItem3 = CartItem(
+        user_id=3,
+        product_id=4,
+        quantity=2,
+    )
+    
+
+    
+    db.session.add(cartItem1)
+    db.session.add(cartItem2)
+    db.session.add(cartItem3)
+
+    db.session.commit()
+
+
+def undo_cartItems():
+    if environment == "production":
+        db.session.execute(
+            f"TRUNCATE table {SCHEMA}.cartItems RESTART IDENTITY CASCADE;")
+    else:
+        db.session.execute("DELETE FROM cartItems")
+
+    db.session.commit()
