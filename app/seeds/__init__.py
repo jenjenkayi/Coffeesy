f8ee885bd35d9b2c2aab3cdee0983fe0be9b9984
Jenny Jiang <jennyjiang0830@hotmail.com>
2023-01-04 14:33:20 -0800

    deploy

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -15,14 +15,16 @@ seed_commands = AppGroup('seed')
 @seed_commands.command('all')
 def seed():
     if environment == 'production':
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.commit()
         # Before seeding in production, you want to run the seed undo 
         # command, which will  truncate all tables prefixed with 
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
-        undo_users()
-        undo_products()
-        undo_reviews()
-        undo_cartItems()
+        # undo_users()
+        # undo_products()
+        # undo_reviews()
+        # undo_cartItems()
     seed_users()
     seed_products()
     seed_reviews()
